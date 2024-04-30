e078412376570c78b861bb6ec3f7721d2c7a7a42
AndresJMir <anunez1@insjoaquimmir.cat>
2024-04-30 20:56:04 +0200

    userResource

--- laravel/app/Http/Controllers/Api/TokenController.php
+++ laravel/app/Http/Controllers/Api/TokenController.php
@@ -7,48 +7,86 @@ use Illuminate\Http\Request;
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
+
 use App\Models\User;
+use App\Models\Role;
 use App\Http\Resources\UserResource;
 
 class TokenController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+    public function user(Request $request)
     {
-        //
+        $user = User::where('email', $request->user()->email)->first();
+
+        return response()->json([
+            "success" => true,
+            "user"    => new UserResource($user),
+            "roles"   => [ $user->role->name ],
+        ]);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
+    public function register(Request $request)
     {
-        //
+        $validatedData = $request->validate([
+            "name"      => "required|string|max:255",
+            "email"     => "required|string|email|max:255|unique:users",
+            "password"  => "required|string|min:8"
+        ]);
+
+        $user = User::create([
+            "name"      => $validatedData["name"],
+            "email"     => $validatedData["email"],
+            "password"  => Hash::make($validatedData["password"]),
+            "role_id"   => Role::where('name', 'USUARIO')->first()->id,
+        ]);
+
+        return $this->_generateTokenResponse($user);
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
+    public function login(Request $request)
     {
-        //
+        $credentials = $request->validate([
+            'email'     => 'required|email',
+            'password'  => 'required',
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            // Get user
+            $user = User::where([
+                ["email", "=", $credentials["email"]]
+            ])->firstOrFail();
+            // Revoke all old tokens
+            $user->tokens()->delete();
+            // Generate new token
+            return $this->_generateTokenResponse($user);
+        } else {
+            return response()->json([
+                "success" => false,
+                "message" => "Invalid login credentials"
+            ], 401);
+        }
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
+    public function logout(Request $request)
     {
-        //
+        // Revoke token used to authenticate current request...
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json([
+            "success" => true,
+            "message" => "Current token revoked",
+        ]);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
+    protected function _generateTokenResponse(User $user)
     {
-        //
+        $token = $user->createToken("authToken")->plainTextToken;
+
+        return response()->json([
+            "success"   => true,
+            "authToken" => $token,
+            "tokenType" => "Bearer"
+        ], 200);
     }
 }
